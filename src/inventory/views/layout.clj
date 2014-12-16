8efd8a6e25d3186c5604679f6d2e3ff7b05adb8a
Koustubh <Keurons@gmail.com>
2014-12-16 19:43:28 +0530

    Added the Renderable protocol to layout
    Added and implemented the Renderable layout.

--- src/inventory/views/layout.clj
+++ src/inventory/views/layout.clj
@@ -1,14 +1,23 @@
 (ns inventory.views.layout
-  (:require [hiccup.page :refer [html5 include-css]]))
-
-(defn common [& body]
-  (html5
-    [:head
-     [:title "CDK Inventory System"]
-     (include-css "/css/screen.css")]
-    [:body body]))
+   (:require [ring.util.response :refer [content-type response]]
+             [compojure.core :refer :all]
+             [compojure.response :refer [Renderable]]
+             [selmer.parser :as parser]
+             [selmer.parser :refer [render-file]]))
 
 
-(def template-folder "inventory/views/templates")
+(def template-folder "inventory/views/templates/")
 
+(defn utf-8-response [html]
+  (content-type (response html) "text/html; charset=utf-8"))
 
+(deftype RenderablePage [template params]
+  Renderable
+  (render [this request]
+          (->> (assoc params
+                 :context (:context request))
+          (parser/render-file (str template-folder template))
+          utf-8-response)))
+
+(defn render [template & [params]]
+  (RenderablePage. template params))
